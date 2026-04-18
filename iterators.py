12e7cd5fe30798b250b3e8aa666160b3892192fa
suarezmanuel <manuelsuarez244@gmail.com>
2026-04-18 11:26:21 +0300

    one iterator to rule them all

--- iterators.py
+++ iterators.py
@@ -1,26 +1,7 @@
 from copy import deepcopy
 from dataclasses import dataclass, field
 import os
-
-class TreeIterator():
-    def __init__(self, root_path, MAX_DEPTH=-1):
-        self.root_path = root_path
-        self.MAX_DEPTH = MAX_DEPTH
-        self.LIMIT_DEPTH = MAX_DEPTH != -1
-
-    def recurse(self, depth, folder_path):
-        if self.LIMIT_DEPTH and depth > self.MAX_DEPTH: return
-        files = os.listdir(folder_path)
-        for f in files:
-            f_path = f"{folder_path}/{f}"
-            if os.path.isdir(f_path):
-                yield f,1
-                yield from self.recurse(depth=depth+1, folder_path=f_path)
-            else:
-                yield f,0
-    
-    def __iter__(self):
-        yield from self.recurse(0, self.root_path)
+from typing import NamedTuple
 
 class TreeAbstractIterator():
 
@@ -33,10 +14,15 @@ class TreeAbstractIterator():
         f: str = ""
         files: list[str] = field(default_factory=list)
 
-    def __init__(self, root_path, MAX_DEPTH=-1, before=None, on_node=None, on_leaf=None, state=None):
+    class YieldResult(NamedTuple):
+        name: str
+        is_node: bool
+
+    def __init__(self, root_path, MAX_DEPTH=-1, RECURSE_HIDDEN=False, before=None, on_node=None, on_leaf=None, state=None):
         self.root_path = root_path
         self.MAX_DEPTH = MAX_DEPTH
         self.LIMIT_DEPTH = MAX_DEPTH != -1
+        self.RECURSE_HIDDEN = RECURSE_HIDDEN
         self.before = before or (lambda _: None)
         self.on_node = on_node or (lambda _: None)
         self.on_leaf = on_leaf or (lambda _: None)
@@ -53,31 +39,15 @@ class TreeAbstractIterator():
             self.state.files = files
 
             self.before(self.state)
-            if os.path.isdir(f_path):
+            if os.path.isdir(f_path) and (self.RECURSE_HIDDEN or '.' not in f_path):
                 save_state = deepcopy(self.state)
                 self.on_node(self.state)
-                yield f,1
+                yield self.YieldResult(name=f, is_node=True)
                 yield from self.recurse(depth=depth+1, folder_path=f_path)
                 self.state = save_state # so actual recursion can happen
             else:
                 self.on_leaf(self.state)
-                yield f,0
+                yield self.YieldResult(name=f, is_node=False)
     
     def __iter__(self):
         yield from self.recurse(0, self.root_path)
-
-class TreeLeafIterator():
-    def __init__(self, root_path):
-        self.root_path = root_path
-
-    def recurse(self, folder_path):
-        files = os.listdir(folder_path)
-        for f in files:
-            f_path = f"{folder_path}/{f}"
-            if os.path.isdir(f_path):
-                yield from self.recurse(f_path)
-            else:
-                yield f
-    
-    def __iter__(self):
-        yield from self.recurse(self.root_path)
--- main.py
+++ main.py
@@ -1,32 +1,8 @@
 # create a graph in memory, an adjacency list
-from iterators import TreeIterator, TreeAbstractIterator, TreeLeafIterator
+from iterators import TreeAbstractIterator
 from dataclasses import dataclass, field
-import os
 
-MAX_TREE_DEPTH = 2
-
-def _tree(depth, lines_arr, paddings, folder_path):
-    
-    files = os.listdir(folder_path)
-    for i, f in enumerate(files):
-        f_path = f"{folder_path}/{f}"
-
-        prefix = ""
-        for j in range(len(paddings)):
-            if lines_arr[j]:
-                prefix += '|' + ' '*(paddings[j]-1)
-            else:
-                prefix += ' '*paddings[j]
-        prefix += 'L' + '_'*7
-
-        print(f"{prefix}{f}")
-
-        if os.path.isdir(f_path):
-            does_parent_have_next=(i < (len(files)-1))
-            parent_str_len = 8+len(f)
-            _tree(depth=depth+1, lines_arr=lines_arr+[does_parent_have_next], paddings=paddings+[parent_str_len], folder_path=f_path)
-
-def tree(folder_path):
+def tree(folder_path, MAX_DEPTH=-1):
     if folder_path == "":
         return
     
@@ -56,7 +32,7 @@ def tree(folder_path):
 
     root = str.split(folder_path, '/').pop()
     print(root)
-    titer = TreeAbstractIterator(folder_path, before=before, on_node=on_node, on_leaf=on_leaf, state=CustomState(lines_arr=[False], paddings=[len(root)]))
+    titer = TreeAbstractIterator(folder_path, MAX_DEPTH=MAX_DEPTH, before=before, on_node=on_node, on_leaf=on_leaf, state=CustomState(lines_arr=[False], paddings=[len(root)]))
     for _ in titer:
         pass
 
@@ -65,27 +41,22 @@ def tree(folder_path):
     # _tree(depth=0, lines_arr=[0], paddings=[len(root)], folder_path=folder_path)
 
 def get_leaves(folder_path):
-    titer = TreeLeafIterator(folder_path)
-    result = []
-    for i in titer:
-        result += [i]
-    return result
+    titer = TreeAbstractIterator(folder_path)
+    return [name for name, is_node in titer if not is_node]
 
 def find_references(file_name, folder_path):
     pass
     
 def build_adjacency_list(folder_path):
     leaves = get_leaves(folder_path)
-    list = {}
-    for l in leaves:
-        list[l] = []
+    list = {l:[] for l in leaves}
     # list = populate_adjacency_list(list, folder_path)
     return list
 
 
-tree("/Users/manuel/Code/minecraft")
+tree("/Users/manuel/Code/visualizer", MAX_DEPTH=0)
 
-# print(get_leaves("/Users/manuel/Code/minecraft"))
+# print(get_leaves("/Users/manuel/Code/visualizer/test"))
 
 # titer = TreeAbstractIterator("/Users/manuel/Code/visualizer", MAX_DEPTH=0, on_leaf=lambda state: state.update({"count": state["count"]+1}), on_node=lambda *args: None, state={"count": 0})
 # for f in titer:
